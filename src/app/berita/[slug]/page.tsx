16757e10e12fb602e86d425d4d7210cf640fc57f
NataKun7 <natakun7@gmail.com>
2026-05-15 21:31:35 +0700

    Change Hero image height to 500px on mobile in news detail

--- src/app/berita/[slug]/page.tsx
+++ src/app/berita/[slug]/page.tsx
@@ -88,7 +88,7 @@ export default function BeritaDetailPage() {
         transition={{ duration: 0.6 }}
         className="p-[20px] w-full"
       >
-        <div className="relative h-[600px] md:h-[700px] overflow-hidden rounded-[20px] shadow-sm">
+        <div className="relative h-[500px] md:h-[700px] overflow-hidden rounded-[20px] shadow-sm">
           <img 
             src={newsDetail.coverImage} 
             alt={newsDetail.title} 
