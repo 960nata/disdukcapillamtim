41cc4cb2589366f3693c2efd9add937230706002
NataKun7 <natakun7@gmail.com>
2026-05-17 17:23:31 +0700

    fix: update Batanghari coordinates to exact geographic office location

--- src/components/LocationSection.tsx
+++ src/components/LocationSection.tsx
@@ -4,7 +4,7 @@ import { useEffect, useRef } from 'react';
 
 const locations = [
   { id: 1, name: "MPP LAMPUNG TIMUR", lat: -5.0188279, lng: 105.5353965, type: "Pusat", image: "/images/foto_kegiatan/pelayanan_ktp.avif", mapUrl: "https://www.google.com/maps/place/Mal+Pelayanan+Publik+(MPP)+Kabupaten+Lampung+Timur/@-5.0345837,105.5422375,12.5z/data=!4m12!1m5!3m4!2zNcKwMDYnMzYuMCJTIDEwNcKwMzInMjQuMCJF!8m2!3d-5.11!4d105.54!3m5!1s0x2e409b0032466e6f:0x140b8000f6895559!8m2!3d-5.0188279!4d105.5353965!16s%2Fg%2F11xf8nn920?entry=ttu&g_ep=EgoyMDI2MDUxMS4wIKXMDSoASAFQAw%3D%3D" },
-  { id: 2, name: "Kecamatan Batanghari", lat: -5.1884, lng: 105.4183, type: "Kecamatan", image: "/images/foto_kegiatan/jemput_bola.avif" },
+  { id: 2, name: "Kecamatan Batanghari", lat: -5.1515845, lng: 105.3716684, type: "Kecamatan", image: "/images/foto_kegiatan/jemput_bola.avif" },
   { id: 3, name: "Kecamatan Way Jepara", lat: -5.1450, lng: 105.7330, type: "Kecamatan", image: "/images/foto_kegiatan/penyerahan_ktp.avif" },
   { id: 4, name: "Kecamatan Mataram Baru", lat: -5.2480, lng: 105.7265, type: "Kecamatan", image: "/images/foto_kegiatan/sosialisasi_sekolah.avif" },
   { id: 5, name: "Kecamatan Labuhan Maringgai", lat: -5.3175, lng: 105.8020, type: "Kecamatan", image: "/images/foto_kegiatan/pelayanan_online.avif" },
